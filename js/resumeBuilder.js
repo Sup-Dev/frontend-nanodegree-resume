8990150648e8b4b456d24d3d3e086ce615875157
Sup-Dev <sourabh.dev90@gmail.com>
2016-03-12 22:24:31 +0530

    feat: added name and role into the header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "Sourabh Dev");
+var formattedRole = HTMLheaderRole.replace("%data%", "Software Architect");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
