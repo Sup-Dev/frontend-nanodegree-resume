da1e7f2abea48f8ffbc8bf0d3165a017a552e045
Sup-Dev <sourabh.dev90@gmail.com>
2016-03-13 23:07:56 +0530

    feat: added work details

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -48,3 +48,44 @@ var projects = {
     }
   ]
 };
+
+
+// Add to resume
+
+// Bio
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+
+// Skills
+if (bio.skills.length > 0) {
+  $("#header").append(HTMLskillsStart);
+  var formattedSkills;
+  
+  for (var i=0; i<bio.skills.length; i++) {
+    formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+    $("#skills").append(formattedSkills);
+  }
+}
+
+// Work
+if (work.jobs.length > 0) {
+  $("#workExperience").append(HTMLworkStart);
+    
+  for (job in work.jobs) {
+    var employer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var title = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    
+    $(".work-entry:last").append(employer + title);
+    $(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[job].dates));
+    $(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[job].description));
+  }
+}
