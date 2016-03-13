aa4d479ec6e844d5437d3a33536b83a67cfed7be
Sup-Dev <sourabh.dev90@gmail.com>
2016-03-13 08:15:46 +0530

    feat: added new objects to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,50 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var formattedName = HTMLheaderName.replace("%data%", "Sourabh Dev");
-var formattedRole = HTMLheaderRole.replace("%data%", "Software Architect");
+var bio = {
+  "name": "Sourabh Dev",
+  "role": "Software Architect",
+  "contacts": {
+    "mobile": "999-999-9999",
+    "email": "sourabh@test.com",
+    "github": "Sup-Dev",
+    "twitter": "@sevenzone",
+    "location": "Gurgaon, India"
+  },
+  "welcomeMessage": "Hello World!",
+  "skills": ["programming", "planning", "entertaining"]
+};
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var eduaction = {
+  "schools": [
+    {
+      "name": "Indraprast University",
+      "city": "New Delhi, India",
+      "degree": "Bachelors",
+      "mojors": ["CS"],
+      "dates": 2014,
+      "url": "http://test.com"
+    }
+  ]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Ubinext Software",
+      "title": "Software Architect",
+      "dates": "May 2014 - Current",
+      "description": "I work as a computer architect here!"
+    }
+  ]
+};
+
+var projects = {
+  "projects": [
+    {
+      "title": "Infrastitch",
+      "dates": "Dec 2014",
+      "description": "A project managemant software"
+    }
+  ]
+};
